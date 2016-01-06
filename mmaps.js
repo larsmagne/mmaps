61bb12a96b5642dbde4eb744ef6381fd45e44894
Lars Magne Ingebrigtsen <larsi@gnus.org>
2016-01-06 13:39:33 +0100

    Use geocharts instead of google maps since google maps
    doesn't allow marking countries.

--- mmaps.js
+++ mmaps.js
@@ -1,68 +1,42 @@
-function initMap() {
-  // Specify features and elements to define styles.
-  var styleArray = [
-    {
-      featureType: "road",
-      elementType: "all",
-      stylers: [
-	{ visibility: "off" }
-      ]
-    },{
-      featureType: "administrative",
-      elementType: "all",
-      stylers: [
-	{ visibility: "off" }
-      ]
-    },{
-      featureType: "landscape.natural.terrain",
-      elementType: "all",
-      stylers: [
-	{ visibility: "off" }
-      ]
-    },{
-      featureType: "landscape.natural.landcover",
-      elementType: "all",
-      stylers: [
-	{ visibility: "off" }
-      ]
-    },{
-      featureType: "poi",
-      elementType: "all",
-      stylers: [
-	{ visibility: "off" }
-      ]
-    },{
-      featureType: "water",
-      elementType: "labels",
-      stylers: [
-	{ visibility: "off" }
-      ]
-    },{
-      featureType: "water",
-      elementType: "all",
-      stylers: [
-	{ color: "#000000" }
-      ]
-    },{ 
-      featureType: "landscape", 
-      elementType: "geometry", 
-      stylers: [ 
-	{ color: "#000000" }
-      ] 
-    } 
+var films = [
+  ["de", "Die Patronin", "http://lars.ingebrigtsen.no/foo/de"],
+  ["ru", "Mirror", "http://lars.ingebrigtsen.no/foo/ru"],
+  ["se", "Sångar från andre våningen", "http://lars.ingebrigtsen.no/foo/se"]
+];
 
-  ];
+function drawRegionsMap() {
+  var table = [['Country', 'Newness']];
+  var points = 700;
 
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 30, lng: 0},
-    scrollwheel: false,
-    // Apply the map style array to the map.
-    styles: styleArray,
-    zoom: 2,
-    streetViewControl: false,
-    zoomControl: false,
-    mapTypeControlOptions: { mapTypeIds: [] } 
+  $.map(films, function(film) {
+    table.push([film[0], points]);
+    if (points > 300)
+      points -= 10;
   });
+
+  console.log(table);
+  
+  var data = google.visualization.arrayToDataTable(table);
+
+  var options = {
+    backgroundColor: "#000000",
+    datalessRegionColor: "#000000",
+    tooltip: { trigger: "none" }
+  };
+
+  var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+  chart.draw(data, options);
+  google.visualization.events.addListener(chart, 'select', function selectHandler() {
+    var selected = chart.getSelection()[0];
+    if (! selected)
+      return;
+    console.log(selected.row);
+  });
+}
+
+function initMap() {
+  google.charts.load('current', {'packages':['geochart']});
+  google.charts.setOnLoadCallback(drawRegionsMap);
 }
 
