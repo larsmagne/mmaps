a57f7870970bf1811914cb6d5a7097e2d63beaa7
Lars Magne Ingebrigtsen <larsi@gnus.org>
2016-01-06 14:10:56 +0100

    Prettify the map

--- mmaps.js
+++ mmaps.js
@@ -1,6 +1,6 @@
 var films = [
   ["de", "Die Patronin", "http://lars.ingebrigtsen.no/foo/de"],
-  ["ru", "Mirror", "http://lars.ingebrigtsen.no/foo/ru"],
+  ["ru", "Mirror", "http://lars.ingebrigtsen.no/foo/ru", "1976", "Andrei Tarkovski"],
   ["se", "Sångar från andre våningen", "http://lars.ingebrigtsen.no/foo/se"]
 ];
 
@@ -14,14 +14,14 @@ function drawRegionsMap() {
       points -= 10;
   });
 
-  console.log(table);
-  
   var data = google.visualization.arrayToDataTable(table);
 
   var options = {
     backgroundColor: "#000000",
     datalessRegionColor: "#000000",
-    tooltip: { trigger: "none" }
+    colorAxis: {colors: ['#505050', '#00ff00']},
+    tooltip: { trigger: "none" },
+    legend: "none"
   };
 
   var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
@@ -31,7 +31,7 @@ function drawRegionsMap() {
     var selected = chart.getSelection()[0];
     if (! selected)
       return;
-    console.log(selected.row);
+    displayFilm(films[selected.row]);
   });
 }
 
@@ -40,3 +40,23 @@ function initMap() {
   google.charts.setOnLoadCallback(drawRegionsMap);
 }
 
+function displayFilm(film) {
+  var html = "<a href=\"" + film[2] + "\">" + film[1] +
+	" (" + film[3] + ") by "
+	+ film[4] + "</a>";
+  var box = document.createElement("div");
+  box.style.position = "absolute";
+  box.style.left = $(window).width() / 2 - 150 + "px";
+  box.style.top = $(window).height() / 2 - 25 + "px";
+  box.style.height = "50px";
+  box.style.width = "300px";
+  box.style.display = "block";
+  box.style.background = "#f0f0f0";
+  box.style.color = "black";
+  box.className = "event-lightbox";
+  box.innerHTML = html + "<div class='close'><span>Close</span></div>";
+  document.body.appendChild(box);
+  $(".close").bind("click", function() {
+    $(box).remove();
+  });
+}
