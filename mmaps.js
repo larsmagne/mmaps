2e2a4ba0e74b4553559e79fed6017891057bdffd
Lars Ingebrigtsen <larsi@gnus.org>
2016-03-31 22:27:04 +0200

    Remove the borders between countries

--- mmaps.js
+++ mmaps.js
@@ -44,6 +44,11 @@ function drawRegionsMap() {
   // Remove the zebra stripes in India.
   setTimeout(function() {
       $("#_ABSTRACT_RENDERER_ID_0").remove();
+      $("path").each(function(index) {
+	  // Erase the borders between the countries.
+	  if (this.getAttribute("fill") == "#002000")
+	      this.setAttribute("stroke", "#002000");
+      });
   }, 100);
 }
 
